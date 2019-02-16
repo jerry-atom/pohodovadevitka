5c7618dc8bffecc63bfe4546a250eb376dbffffd
Jaroslav Novotný <jaroslav.novotny.84@gmail.com>
2019-02-16 18:16:02 +0100

    icons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -34,24 +34,24 @@ const TemplateWrapper = ({ children }) => (
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/img/apple-touch-icon.png"
+            href="/uploads/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
-            href="/img/favicon-32x32.png"
+            href="/uploads/favicon-32x32.png"
             sizes="32x32"
           />
           <link
             rel="icon"
             type="image/png"
-            href="/img/favicon-16x16.png"
+            href="/uploads/favicon-16x16.png"
             sizes="16x16"
           />
 
           <link
             rel="mask-icon"
-            href="/img/safari-pinned-tab.svg"
+            href="/uploads/safari-pinned-tab.svg"
             color="#ffffff"
           />
           <meta name="theme-color" content="#fff" />
@@ -59,7 +59,7 @@ const TemplateWrapper = ({ children }) => (
           <meta property="og:type" content="website" />
           <meta property="og:title" content={data.site.siteMetadata.title} />
           <meta property="og:url" content="/" />
-          <meta property="og:image" content="/img/og-image.png" />
+          <meta property="og:image" content="/uploads/og-image.png" />
         </Helmet>
 
         <TopMenu />
